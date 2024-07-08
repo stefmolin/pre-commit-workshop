baf06d1ef3f5cb6321c7f852940c53cb04598cdd
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2024-07-08 16:09:24 +0200

    Add CLI

---
+++ src/filename_validation/cli.py
@@ -0,0 +1,24 @@
+import argparse
+from typing import Sequence
+
+from .validate_filename import validate_filename
+
+
+def main(argv: Sequence[str] | None = None) -> int:
+    parser = argparse.ArgumentParser(prog='validate-filename')
+    parser.add_argument(
+        'filenames',
+        nargs='*',
+        help='Filenames to process.',
+    )
+    parser.add_argument(
+        '--min-len',
+        default=3,
+        type=int,
+        help='Minimum length for a filename.',
+    )
+
+    args = parser.parse_args(argv)
+
+    results = [validate_filename(filename, args.min_len) for filename in args.filenames]
+    return int(any(results))
