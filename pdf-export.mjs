77fe4798cca71a0bf0802ea05dc6bf388ab5c138
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-06-08 14:29:20 -0400

    Try sleeping before generating the PDF

--- pdf-export.mjs
+++ pdf-export.mjs
@@ -8,12 +8,13 @@ const slidesPath = path.join(
   path.dirname(import.meta.url),
   "index.html?print-pdf"
 );
-console.log(`Navigating to ${slidesPath}`);
 
+console.log(`Navigating to ${slidesPath}`);
 await page.goto(slidesPath);
 
-console.log(`Generating PDF version of the slides for ${await page.title()}`);
+await new Promise((r) => setTimeout(r, 2000));
 
+console.log(`Generating PDF version of the slides for ${await page.title()}`);
 await page.pdf({ path: "slides.pdf", format: "A4", timeout: 0 });
 
 await browser.close();
