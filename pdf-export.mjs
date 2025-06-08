0f15863b2a3140430a6e47074b27790e08d80239
Stefanie Molin <24376333+stefmolin@users.noreply.github.com>
2025-06-08 14:25:35 -0400

    Add debugging calls

--- pdf-export.mjs
+++ pdf-export.mjs
@@ -4,9 +4,15 @@ import * as path from "path";
 const browser = await puppeteer.launch();
 const page = await browser.newPage();
 
-await page.goto(
-  path.join(path.dirname(import.meta.url), "index.html?print-pdf")
+const slidesPath = path.join(
+  path.dirname(import.meta.url),
+  "index.html?print-pdf"
 );
+console.log(`Navigating to ${slidesPath}`);
+
+await page.goto(slidesPath);
+
+console.log(`Generating PDF version of the slides for ${await page.title()}`);
 
 await page.pdf({ path: "slides.pdf", format: "A4", timeout: 0 });
 
